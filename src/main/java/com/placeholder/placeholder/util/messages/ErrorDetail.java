df463d62d73070f22575588999fc93d516ac14ba
Sinbelisk <rafjimray21y@gmail.com>
2025-04-25 12:34:23 +0200

    DTO models for errores and tetails

--- src/main/java/com/placeholder/placeholder/util/messages/ErrorDetail.java
+++
@@ -1,7 +0,0 @@
-package com.placeholder.placeholder.util.messages;
-
-public record ErrorDetail(
-        String field,
-        String message,
-        Object rejectedValue
-) {}
---
+++ src/main/java/com/placeholder/placeholder/util/messages/error/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.placeholder.placeholder.util.messages.error;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.util.List;
+
+/**
+ * Standard DTO response message for errors
+ * @param detailedMessage a detailed description of the error.
+ * @param simpleMessage a simplified version of the error.
+ * @param errors optional value, contains a list of {@link ErrorDetail} for more information
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record ErrorResponse(
+        String detailedMessage,
+        String simpleMessage,
+        List<ErrorDetail> errors
+) {}
