ac5ded28a4694840d4fc8a2452582590945bcc68
lPhiNix <pablomargamer05@gmail.com>
2025-05-12 16:37:02 +0200

    Minimal Changes

--- src/main/java/com/placeholder/placeholder/api/controller/MathExpressionController.java
+++ src/main/java/com/placeholder/placeholder/api/controller/MathExpressionController.java
@@ -2,9 +2,9 @@ package com.placeholder.placeholder.api.controller;
 
 import com.placeholder.placeholder.api.dto.ExpressionRequest;
 import com.placeholder.placeholder.api.services.MathExpressionService;
-import com.placeholder.placeholder.db.models.dto.UserCreationRequest;
-import com.placeholder.placeholder.util.enums.AppCode;
+import com.placeholder.placeholder.util.messages.ApiResponseFactory;
 import com.placeholder.placeholder.util.messages.dto.ApiResponse;
+import com.placeholder.placeholder.util.messages.dto.content.EmptyContentResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,18 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/math_expression")
 public class MathExpressionController {
+
     private final MathExpressionService service;
+    private final ApiResponseFactory apiResponseFactory;
     
-    public MathExpressionController(MathExpressionService service) {
+    public MathExpressionController(MathExpressionService service, ApiResponseFactory apiResponseFactory) {
         this.service = service;
+        this.apiResponseFactory = apiResponseFactory;
     }
 
     @PostMapping("/evaluate")
-    public ResponseEntity<ApiResponse<?>> getEvaluatedExpression(@RequestBody ExpressionRequest expressionRequest, HttpServletRequest httpServletRequest) {
+    public ResponseEntity<ApiResponse<EmptyContentResponse>> evaluate(@RequestBody ExpressionRequest expressionRequest, HttpServletRequest httpServletRequest) {
         String result = service.evaluate(expressionRequest.expression());
 
-        ApiResponse<>
-
-        return new ResponseEntity<>(AppCode.OK.getStatus());
+        return apiResponseFactory.ok(httpServletRequest.getRequestURI(), result);
     }
 }
--- src/main/java/com/placeholder/placeholder/api/dto/ExpressionResultResponse.java
+++ src/main/java/com/placeholder/placeholder/api/dto/ExpressionResultResponse.java
@@ -1,8 +1,9 @@
 package com.placeholder.placeholder.api.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.placeholder.placeholder.util.messages.dto.content.MessageContent;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record ExpressionResultResponse(
 
-) {}
+) implements MessageContent {}
