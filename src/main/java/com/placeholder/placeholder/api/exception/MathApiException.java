8f3b25749d969b7089d2227794c6dcdbd8a83b6e
lPhiNix <pablomargamer05@gmail.com>
2025-05-12 17:15:45 +0200

    Math Api Custom Exception and Handler Created

---
+++ src/main/java/com/placeholder/placeholder/api/exception/MathApiException.java
@@ -0,0 +1,11 @@
+package com.placeholder.placeholder.api.exception;
+
+public class MathApiException extends Throwable {
+    public MathApiException(String message) {
+        super(message);
+    }
+
+    public MathApiException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/com/placeholder/placeholder/api/exception/MathApiExceptionGlobalHandler.java
@@ -0,0 +1,8 @@
+package com.placeholder.placeholder.api.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class MathApiExceptionGlobalHandler {
+
+}
