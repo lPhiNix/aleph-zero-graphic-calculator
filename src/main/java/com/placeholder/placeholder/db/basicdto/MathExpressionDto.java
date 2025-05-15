ec07210d6715df6b500e58f5c313c877f9bcc9df
Sinbelisk <rafjimray21y@gmail.com>
2025-05-15 23:56:27 +0200

    minor changes

--- src/main/java/com/placeholder/placeholder/db/basicdto/MathExpressionDto.java
+++ src/main/java/com/placeholder/placeholder/db/basicdto/MathExpressionDto.java
@@ -1,7 +1,5 @@
 package com.placeholder.placeholder.db.basicdto;
 
-import lombok.Value;
-
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.Map;
@@ -9,11 +7,10 @@ import java.util.Map;
 /**
  * DTO for {@link com.placeholder.placeholder.db.models.MathExpression}
  */
-@Value
-public class MathExpressionDto implements Serializable {
-    Integer id;
-    String expression;
-    Map<String, Object> pointsSnapshot;
-    Instant createdAt;
-    Instant updatedAt;
+public record MathExpressionDto(
+        Integer id, String expression,
+        Map<String, Object> pointsSnapshot,
+        Instant createdAt,
+        Instant updatedAt
+) implements Serializable {
 }
