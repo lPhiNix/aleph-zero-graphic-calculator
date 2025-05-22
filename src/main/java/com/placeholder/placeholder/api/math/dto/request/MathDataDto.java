7a4f2cbd72d849658a7d2d1981be796ddb587350
lPhiNix <pablomargamer05@gmail.com>
2025-05-22 17:13:57 +0200

    Minimal Changes

--- src/main/java/com/placeholder/placeholder/api/math/dto/request/MathDataDto.java
+++ src/main/java/com/placeholder/placeholder/api/math/dto/request/MathDataDto.java
@@ -1,9 +1,5 @@
 package com.placeholder.placeholder.api.math.dto.request;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
-@Data @EqualsAndHashCode
 public record MathDataDto(
     int decimals,
     String origin,
