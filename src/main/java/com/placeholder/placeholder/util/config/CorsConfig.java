dd80d4e4999f406502ebb22166cfc6f96efce28d
Sinbelisk <rafjimray21y@gmail.com>
2025-06-02 13:17:09 +0200

    added CORS configuration for the frontend URL

--- src/main/java/com/placeholder/placeholder/util/config/CorsConfig.java
+++ src/main/java/com/placeholder/placeholder/util/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.placeholder.placeholder.util.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -10,10 +11,13 @@ import java.util.List;
 
 @Configuration
 public class CorsConfig {
+    @Value("${squip.frontend.url}")
+    private String frontendUrl;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("")); //Frontend URL
+        configuration.setAllowedOrigins(List.of(frontendUrl)); //Frontend URL
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
