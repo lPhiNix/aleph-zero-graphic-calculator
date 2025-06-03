36381ce03c72c2ff967ff46fa13c5dbc611b8249
Sinbelisk <rafjimray21y@gmail.com>
2025-06-04 00:24:35 +0200

    utility class for creating URIs

--- src/main/java/com/placeholder/placeholder/util/filters/NoSecurityConfig.java
+++ src/main/java/com/placeholder/placeholder/util/filters/NoSecurityConfig.java
@@ -5,10 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+/**
+ * Security configuration class for disabling security features.
+ * This configuration allows all requests without any authentication or authorization.
+ *
+ * WARNING: This configuration should only be used in non-production environments
+ */
 @Configuration
 @Profile("no-security")
 public class NoSecurityConfig {
