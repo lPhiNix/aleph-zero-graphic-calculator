9985caab4b1585e50a333cba8117bedd2fd7b985
Sinbelisk <rafjimray21y@gmail.com>
2025-06-08 16:21:32 +0200

    Fixed a cagada

---
+++ src/main/java/com/placeholder/placeholder/db/basicdto/HistoryExpressionDto.java
@@ -0,0 +1,7 @@
+package com.placeholder.placeholder.db.basicdto;
+
+/**
+ * DTO for {@link com.placeholder.placeholder.db.models.HistoryExpression}
+ */
+public record HistoryExpressionDto(){
+  }
---
+++ src/main/java/com/placeholder/placeholder/db/models/HistoryExpression.java
@@ -0,0 +1,7 @@
+package com.placeholder.placeholder.db.models;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class HistoryExpression {
+  }
